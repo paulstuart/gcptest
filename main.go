19109aa3e5c770c114bcd8f495d7d7df9066c5a9
Paul Stuart <pauleyphonic@gmail.com>
2016-11-26 16:28:24 -0800

    comment out auth hack

--- main.go
+++ main.go
@@ -4,10 +4,14 @@ import (
 	"crypto/tls"
 	"fmt"
 	"io"
+	//"io/ioutil"
 	"log"
 	"net/http"
+	//"os"
 	"strings"
 	"time"
+	//"golang.org/x/oauth2"
+	//"golang.org/x/net/context"
 )
 
 var (
@@ -15,6 +19,43 @@ var (
 	logName = "bucket-log"
 	lggr    = &gLogger{}
 )
+
+const ignore = `
+func creds() {
+	file := os.Getenv("GOOGLE_APPLICATION_CREDENTIALS")
+	/*
+		if len(file) > 0 {
+			data, err := ioutil.ReadFile(file)
+			if err != nil {
+				log.Fatalln("file:", err)
+			}
+			conf, err := oauth.ConfigFromJSON(data)
+			if err != nil {
+				log.Fatalln("conf:", err)
+			}
+		}
+	*/
+
+	// Warning: The better way to use service accounts is to set GOOGLE_APPLICATION_CREDENTIALS
+	// and use the Application Default Credentials.
+	ctx := context.Background()
+	// Use a JSON key file associated with a Google service account to
+	// authenticate and authorize.
+	// Go to https://console.developers.google.com/permissions/serviceaccounts to create
+	// and download a service account key for your project.
+	//
+	// Note: The example uses the datastore client, but the same steps apply to
+	// the other client libraries underneath this package.
+	client, err := datastore.NewClient(ctx,
+		projID,
+		option.WithServiceAccountFile(file))
+	if err != nil {
+		// TODO: handle error.
+	}
+	// Use the client.
+	_ = client
+}
+`
 
 func init() {
 	var err error
